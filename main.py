99615b293c6dec1c85fcdbe71d64734755e0e214
xd1020 <48640038+xd1020@users.noreply.github.com>
2026-02-01 17:38:28 +0300

    Add fortune endpoint

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"status": "Coffee Fortune API is running ☕"}
+
+from fastapi import File, UploadFile
+
+@app.post("/fortune")
+async def fortune(file: UploadFile = File(...)):
+    return {
+        "message": "Fotoğraf alındı ☕ Fal yorumuna hazırız!"
+    }
+    
